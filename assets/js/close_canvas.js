1c08d4c8b59b81852c24d4694301ff69160db640
pablocandia92 <pablocandia395@gmail.com>
2024-11-11 13:35:47 -0300

    fix close_canvas.js

--- assets/js/close_canvas.js
+++ assets/js/close_canvas.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.navbar-nav .nav-link').forEach(item => {
         item.addEventListener('click', () => {
-            console.log('Nav item clicked');
             const offcanvasElement = document.getElementById('offcanvasNavbar');
-            const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement); // Get the existing instance
+            try {
+                const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+    
+                // If no instance exists, create one
+                if (!offcanvas) {
+                    offcanvas = new bootstrap.Offcanvas(offcanvasElement);
+                }
+    
+                offcanvas.hide();
+            } catch {
 
-            // If no instance exists, create one
-            if (!offcanvas) {
-                offcanvas = new bootstrap.Offcanvas(offcanvasElement);
             }
-
-            offcanvas.hide(); // Hide the offcanvas
         });
     });
 });
